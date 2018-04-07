26c65bc40de09f4cb51a8f3c86f1f9f252319982
Shigeki Masuda <macha3162@gmail.com>
2018-04-07 18:02:34 +0900

    update config.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,11 @@ Sidekiq.configure_server do |config|
     config.redis = {url: 'redis://localhost:6379', namespace: 'sidekiq'}
   end
 end
+
+Sidekiq.configure_client do |config|
+  if Rails.env.production?
+    config.redis = {url: ENV['REDISTOGO_URL'], namespace: 'sidekiq'}
+  else
+    config.redis = {url: 'redis://localhost:6379', namespace: 'sidekiq'}
+  end
+end
