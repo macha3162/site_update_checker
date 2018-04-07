2c7843576897155396ead8075acd1e9195d7a37c
Shigeki Masuda <macha3162@gmail.com>
2018-04-07 18:10:39 +0900

    update config.

--- db/migrate/20180121062522_create_site_versions.rb
+++ db/migrate/20180121062522_create_site_versions.rb
@@ -1,6 +1,6 @@
 class CreateSiteVersions < ActiveRecord::Migration[5.1]
   def change
-    create_table :site_diffs do |t|
+    create_table :site_versions do |t|
       t.integer :site_id
       t.string :checksum
       t.integer :status_code
@@ -8,6 +8,6 @@ class CreateSiteVersions < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
-    add_index :site_diffs, :site_id
+    add_index :site_versions, :site_id
   end
 end
