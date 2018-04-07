64b46d638cae8373186305865b2548db8b2557b1
Shigeki Masuda <macha3162@gmail.com>
2018-04-07 17:42:47 +0900

    chenge redis server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
-  if Rails.env.production? && ENV['REDIS_URL']
-    config.redis = {url: ENV['REDIS_URL'], namespace: 'sidekiq'}
+  if Rails.env.production? && ENV['REDISCLOUD_URL']
+    config.redis = {url: ENV['REDISCLOUD_URL'], namespace: 'sidekiq'}
   else
     config.redis = {url: 'redis://localhost:6379', namespace: 'sidekiq'}
   end
